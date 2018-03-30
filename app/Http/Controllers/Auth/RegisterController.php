929d591f8740a5ec790d7cca72677625ddde79dc
Ken Vilar <kenvilar@gmail.com>
2018-03-30 16:24:38 +0800

    Add username

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,6 +51,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
+            'username' => 'required|string|min:6',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
@@ -64,9 +65,10 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+	        'name'     => $data['name'],
+	        'email'    => $data['email'],
+	        'username' => $data['username'],
+	        'password' => Hash::make($data['password']),
         ]);
     }
 }
